770ff1c0afb9641a5e6e0f610c4081975fa3b051
Aramis jaime <you@example.com>
2023-06-02 19:55:07 -0300

    add url to getUpcomingMovies

--- src/utils/addonsUrls.ts
+++ src/utils/addonsUrls.ts
@@ -1,5 +1,6 @@
 export const baseUrl = `https://api.themoviedb.org/3`
 
-export  const urlToGetTopRatedMovies = `/movie/top_rated?language=en-US&page=`
-export  const urlToGetPopularMovies = `/movie/popular?language=en-US&page=`
-export  const urlTogetMovieToExplorer = `/movie/top_rated?language=en-US&page=`
+export const urlToGetTopRatedMovies = `/movie/top_rated?language=en-US&page=`
+export const urlToGetPopularMovies = `/movie/popular?language=en-US&page=`
+export const urlTogetMovieToExplorer = `/movie/top_rated?language=en-US&page=`
+export const urlGetUpcomingMovies = `/movie/upcoming?language=en-US&page=`
