f77719915553e0c0e86630d4b0dc64027259fc66
Aramis jaime <you@example.com>
2023-06-02 15:31:04 -0300

    add another url to create other request

--- src/utils/addonsUrls.ts
+++ src/utils/addonsUrls.ts
@@ -1,1 +1,5 @@
 export const baseUrl = `https://api.themoviedb.org/3`
+
+export  const urlToGetTopRatedMovies = `/movie/top_rated?language=en-US&page=`
+export  const urlToGetPopularMovies = `/movie/popular?language=en-US&page=`
+export  const urlTogetMovieToExplorer = `/movie/top_rated?language=en-US&page=`
