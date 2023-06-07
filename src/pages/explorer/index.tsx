c44b86d4b6b85d592b53e54f766ee2b757744da0
Aramis jaime <you@example.com>
2023-06-07 12:34:33 -0300

    i delete innecesary imports

--- src/pages/explorer/index.tsx
+++ src/pages/explorer/index.tsx
@@ -1,5 +1,4 @@
 import { serviceGetMoviesToExplorer } from "@/services/movies";
-import React, { useRef } from "react";
 import styles from "../../styles/explorer.module.scss";
 import RenderMovies from "@/components/renderMovieList/renderMovieList";
 
