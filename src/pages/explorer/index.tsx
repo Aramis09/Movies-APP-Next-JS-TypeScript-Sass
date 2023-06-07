c949058187da7999408644c64db1195b5a1c13fa
Aramis jaime <you@example.com>
2023-06-07 12:34:17 -0300

    i delete innecesary imports

--- src/pages/explorer/index.tsx
+++ src/pages/explorer/index.tsx
@@ -1,16 +1,7 @@
-import Movie from "@/components/movie/movie";
-import useToDoRequest from "@/customHooks/useToDoRequest";
-import {
-  handleChangePage,
-  serviceGetMoviesToExplorer,
-} from "@/services/movies";
+import { serviceGetMoviesToExplorer } from "@/services/movies";
 import React, { useRef } from "react";
 import styles from "../../styles/explorer.module.scss";
 import RenderMovies from "@/components/renderMovieList/renderMovieList";
-const paramsHook = {
-  service: serviceGetMoviesToExplorer,
-  serviceParams: { page: "1" },
-};
 
 export default function Explorer() {
   return (
