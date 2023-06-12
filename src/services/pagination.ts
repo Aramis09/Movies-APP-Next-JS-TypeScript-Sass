2be4134d5ec5f8362e9ce1c5e4e06c2518945d59
Aramis jaime <you@example.com>
2023-06-12 20:00:47 -0300

    i improve pagination, now always it show ten numbers

--- src/services/pagination.ts
+++ src/services/pagination.ts
@@ -1,18 +1,14 @@
 export const createArrayPagination = (currentPage:number)=> {
-  console.log(currentPage);
-  
-  let arrayOfPages:number[] = []
-  if(currentPage <= 10){
-    arrayOfPages = Array.from(
-      { length: Number(currentPage) },
-      (_, current) => current + 1
-    );
-  }
-  if(currentPage > 10 ){
+  //!Esto lo que hace es mostrar la paginacion, siempre muestra 10 numeros y la posicion actual
+  let arrayOfPages :number[]= []
+  if(currentPage){
     arrayOfPages = Array.from(
       { length: 10 },
-      (_, current) => current + (Math.floor(currentPage/10))*10
+      (_, current) => current + ((Math.floor(currentPage/10))*10 ?(Math.floor(currentPage/10))*10:1)
     );
   }
+
+  //!((Math.floor(currentPage/10))*10 ?(Math.floor(currentPage/10))*10:1) esto indica de donde empieza el array y siempre calcula el principio de cada multiplio de 10
+   //todo el condicional es para que no muestre el cero, ya que este no pertenece a un numero valido de la paginacion
   return arrayOfPages
 }
